392232154533d88f79d787c0c13f8c0c9bebafde
Marcin Galiński <marcinggalinski@gmail.com>
2022-09-02 00:58:11 +0200

    init backend

---
+++ backend/CahClone.Web/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CahClone.Web.Controllers;
+
+[ApiController]
+[Route("users")]
+public class UsersController : Controller
+{
+    [HttpGet("is-username-taken/{username}")]
+    public ActionResult<bool> IsUsernameTaken(string username)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ backend/CahClone.Web/Program.cs
@@ -0,0 +1,8 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapControllers();
+
+app.Run();
